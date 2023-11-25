f9f4b9d61cb12083260c92585738d8db611afd86
Alof Bronco <poliakovillia@gmail.com>
2023-11-25 16:36:46 +0200

    Minor updates

--- js/script.js
+++ js/script.js
@@ -2,4 +2,8 @@ $(document).ready(function () {
   $("#button-header").on("click", function () {
     $(".modal").css("display", "flex");
   });
+
+  $(".button-20").on("click", function () {
+    $(".modal").css("display", "none");
+  });
 });
