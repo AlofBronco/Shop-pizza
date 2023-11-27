320aad93ee7327418cf9631f57f2c3ba381ae73c
Alof Bronco <poliakovillia@gmail.com>
2023-11-27 19:31:08 +0200

    Major changes

--- js/script.js
+++ js/script.js
@@ -1,13 +1,64 @@
+//FOR SELECTORS!!!
+let modal = document.querySelector(".modal");
+let grid = document.querySelector(".grid");
+let counter = 0;
+
+//FOR SELECTORS!!!
+
 function goBack() {
   window.location.href = "../html/index.html";
 }
 
 function showModal() {
-  let modal = document.querySelector(".modal");
   modal.style.display = "flex";
 }
 
 function modalHide() {
-  let modal = document.querySelector(".modal");
   modal.style.display = "none";
+}
+
+function uploadItem() {
+  document.querySelector(".button-80").removeAttribute("onclick");
+  document.querySelector(".button-80").setAttribute("onclick", "uploadItem();");
+
+  counter++;
+
+  let title = document.querySelector("#title");
+  let price = document.querySelector("#price");
+  let image = document.querySelector("#fileInput");
+  let main = document.querySelector("main");
+
+  let itemTitle = document.createElement("h1");
+  itemTitle.textContent = title.value;
+
+  let itemPrice = document.createElement("h2"); //CHANGE TO <P> IF NEEDED
+  itemPrice.textContent = price.value;
+
+  let itemImage = document.createElement("img");
+  if (image.files[0]) {
+    itemImage.src = URL.createObjectURL(image.files[0]);
+  } else {
+    console.error("No file selected");
+  }
+
+  if (title.value == "" || price.value == "" || !image.files[0]) {
+    alert("There is No Data");
+    return;
+  }
+
+  let itemHolder = document.createElement("div");
+  itemHolder.className = "item item" + counter;
+
+  itemHolder.append(itemImage, itemTitle, itemPrice);
+
+  grid.append(itemHolder);
+
+  modal.style.display = "none";
+  title.value = "";
+  price.value = "";
+  image.value = "";
+}
+
+function removeText() {
+  document.querySelector(".positioning").remove();
 }
