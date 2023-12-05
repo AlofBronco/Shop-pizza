5d6896b19c37d14fef0d953eddc2aa17d24ce80a
Alof Bronco <poliakovillia@gmail.com>
2023-12-05 21:46:03 +0200

    Items are almost completed

--- js/script.js
+++ js/script.js
@@ -34,9 +34,29 @@ $(document).ready(function () {
     $(".button", parentGroup).removeClass("button-active");
 
     $(this).addClass("button-active");
+  });
 
-    let newPrice = parseFloat($(this).data("price"));
-    $(".price").text(newPrice.toFixed(2) + " грн");
+  $(".button-largest").on("click", function () {
+    $(".button-fluff").removeClass("button-active");
+
+    let parentGroup = $(this).closest(".button-group");
+    $(".button", parentGroup).removeClass("button-active");
+
+    $(".button-fluff").css("cursor", "not-allowed");
+    $(".button-fluff").prop("disabled", true);
+
+    if (!$(".button-fluff").hasClass("button-active")) {
+      $(".button").removeClass("button-active");
+      $(".button-thin").addClass("button-active");
+    }
+  });
+
+  $(".button-smaller").on("click", function () {
+    $(".button-fluff").prop("disabled", false);
+    $(".button-fluff").css("cursor", "pointer");
+
+    $(".button").removeClass("button-active");
+    $(".button-fluff").addClass("button-active");
   });
 });
 
