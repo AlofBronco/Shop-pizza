a3c2e5c3d1a41cf8150050fceb739f489e94d5a9
Alof Bronco <poliakovillia@gmail.com>
2023-12-18 21:52:30 +0200

    Backend changes

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
-import express from "express";
-import path from "path";
+const express = require("express");
+const path = require("path");
+const sqlite3 = require("sqlite3").verbose();
 
-const __dirname = path.resolve();
+let sql;
 const PORT = process.env.PORT ?? 3000;
 const app = express();
 
+// Creating DB
+const db = new sqlite3.Database("./price.db", sqlite3.OPEN_READWRITE, (err) => {
+  if (err) {
+    console.error(err.message);
+  } else {
+    console.log("Connection to DB successful");
+  }
+});
+
+// Running DB
+sql = "CREATE TABLE price(id INTEGER PRIMARY KEY,price INTEGER,type TEXT)";
+db.run(sql);
+
+sql = "INSERT INTO price(price,type) VALUES(?,?)";
+db.run(sql, [100, "fluff"], (err) => {
+  if (err) return console.error(err.message);
+  console.log("Data inserted successfully");
+});
+
+// Endpoints
 app.use(express.static(path.resolve(__dirname, "static")));
 
 app.get("/", (req, res) => {
@@ -19,6 +40,23 @@ app.get("/minigame", (req, res) => {
   res.sendFile(path.resolve(__dirname, "static", "html", "minigame.html"));
 });
 
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, "static", "html", "error404.html"));
+});
+
+// App listener and port
 app.listen(PORT, () => {
-  console.log(`Server is at http://localhost:${PORT}`);
+  console.log(`Server is started at http://localhost:${PORT}`);
+});
+
+// Closing DB
+process.on("SIGINT", () => {
+  db.close((err) => {
+    if (err) {
+      console.error(err.message);
+    } else {
+      console.log("Closing DB successful");
+      process.exit(0);
+    }
+  });
 });
